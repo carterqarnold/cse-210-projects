44b138063bbbfc023876f8193e1b74a2c9aa9c45
CarterQArnold <arn24013@byui.edu>
2025-01-11 18:57:54 -0700

    Finished Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomNumber = new Random();
+        int number = randomNumber.Next(1,100);
+        int guess;
+        int guessNumber = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guessNumber ++;
+
+            string strGuess = Console.ReadLine();
+            guess = int.Parse(strGuess);
+            
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+    
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        } while (guess != number);
+        Console.WriteLine("You guessed it!");
+
+        if (guessNumber != 1)
+        {
+            string guessednumber = guessNumber.ToString();
+            Console.WriteLine($"You guessed {guessNumber} times");
+        }
+
+        else{
+            Console.WriteLine("You guessed it in 1 guess lucky you!");
+        }
     }
 }
