2b252c0e146bc1c3d308886b0b22b9dd10736d2c
CarterQArnold <arn24013@byui.edu>
2025-01-11 18:05:22 -0700

    Finished Exercise

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string strGradePercentage = Console.ReadLine();
+
+        int gradePercentage = int.Parse(strGradePercentage);
+        string letter = "No letter grade available";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (gradePercentage >= 80 && gradePercentage <= 90)
+        {
+            letter = "B";
+        }
+
+        else if (gradePercentage >= 70 && gradePercentage <= 80)
+        {
+            letter = "C";
+        }
+
+        else if (gradePercentage >= 60 && gradePercentage <= 70)
+        {
+            letter = "D";
+        }
+
+        else if (gradePercentage < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"The grade you got was {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("You can do better next time!");
+        }
     }
 }
